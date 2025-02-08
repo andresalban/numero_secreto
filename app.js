67024f91232db44b551ea2d28f8529a4bde21dbc
Andrés Albán <andresalban@unicauca.edu.co>
2025-02-08 15:32:01 +0000

    aula 1 terminada

---
+++ app.js
@@ -0,0 +1,9 @@
+let  titulo = document.querySelector('h1')
+titulo.innerHTML='Juego del número secreto'
+
+let parrafo = document.querySelector('p')
+parrafo.innerHTML='Indica un número de 1 a 100'
+
+function intentoDeUsuario() {
+    alert("¡Hola, has hecho clic en el botón!");
+}
